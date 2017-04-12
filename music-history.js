f2cf7fe29be7860d8d365f4d99d2436863bd4a45
David Brokaw <dgbrokaw2@gmail.com>
2017-04-11 23:36:01 -0400

    put sample data in separate file; convenience wrappers for sql calls; aggregation over date groups

--- music-history.js
+++ music-history.js
@@ -1,82 +1,68 @@
+var print_results = false;
+var insert_data = false;
+
 var db = openDatabase('musictracker', '1.0', 'Music Tracker', 5*1024*1024);
 db.transaction(function(tx) {
 	console.log('creating tables');
-  tx.executeSql('CREATE TABLE IF NOT EXISTS songs (id unique, title, url)', null, function(tx, results) {
-		console.log('success:', results);
-	}, function(tx, error) {
-		console.log('error:', error);
-	});
-  tx.executeSql('CREATE TABLE IF NOT EXISTS listens (date, id)', null, function(tx, results) {
-		console.log('success:', results);
-	}, function(tx, error) {
-		console.log('error:', error);
-	});
+	executeSql(tx, 'CREATE TABLE IF NOT EXISTS songs (id unique, title, url)');
+	executeSql(tx, 'CREATE TABLE IF NOT EXISTS listens (date, id)');
 });
 
-// initSampleData(db);
+if (insert_data) initSampleData(selectAllData);
+else countListensByDate();//selectAllData();
 
-setTimeout(function() {
-	console.log('going to try something now...');
+function initSampleData(callback) {
 	db.transaction(function(tx) {
-		tx.executeSql('SELECT * FROM songs', [], function(tx, results) {
-			console.log(results);
-		});
-		tx.executeSql('SELECT * FROM listens', [], function(tx, results) {
-			console.log(results);
+		executeSql(tx, 'INSERT INTO songs (id, title, url) VALUES (?, ?, ?)', song);
+		listens.forEach(function(listen, idx) {
+			executeSql(tx, 'INSERT INTO listens (date, id) VALUES (?, ?)', [(new Date(listen[0])).toJSON(), listen[1]], (function(idx) {
+				if (idx === listens.length-1) callback();
+		  }).bind(null, idx));
 		});
 	});
-}, 3000);
+}
 
-function initSampleData(db) {
-	var song = ['gfAHzU1vgGI', '_BLADEE_SUBARU_', 'https://www.youtube.com/watch?v=gfAHzU1vgGI']
-	var listens = [ ['Wed Mar 01 2017', 'gfAHzU1vgGI']
-	              , ['Thu Mar 02 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 03 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 03 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 03 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 04 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 04 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 04 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 04 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 04 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 10 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 11 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 11 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 11 2017', 'gfAHzU1vgGI']
-	              , ['Sun Mar 12 2017', 'gfAHzU1vgGI']
-	              , ['Mon Mar 13 2017', 'gfAHzU1vgGI']
-	              , ['Tue Mar 14 2017', 'gfAHzU1vgGI']
-	              , ['Tue Mar 14 2017', 'gfAHzU1vgGI']
-	              , ['Wed Mar 15 2017', 'gfAHzU1vgGI']
-	              , ['Thu Mar 16 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 17 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 17 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 17 2017', 'gfAHzU1vgGI']
-	              , ['Mon Mar 20 2017', 'gfAHzU1vgGI']
-	              , ['Mon Mar 20 2017', 'gfAHzU1vgGI']
-	              , ['Wed Mar 22 2017', 'gfAHzU1vgGI']
-	              , ['Thu Mar 23 2017', 'gfAHzU1vgGI']
-	              , ['Fri Mar 24 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 25 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 25 2017', 'gfAHzU1vgGI']
-	              , ['Sat Mar 25 2017', 'gfAHzU1vgGI'] ];
-
+function selectAllData() {
 	db.transaction(function(tx) {
-		console.log('inserting song');
-		tx.executeSql('INSERT INTO songs (id, title, url) VALUES (?, ?, ?)', song, function(tx, results) {
-			console.log('success:', results);
-		}, function(tx, error) {
-			console.log('error:', error);
-		});
-
-		console.log('inserting listens');
-		listens.forEach(function(listen) {
-			console.log('inserting:', [(new Date(listen[0])).toJSON(), listen[1]]);
-			tx.executeSql('INSERT INTO listens (date, id) VALUES (?, ?)', [(new Date(listen[0])).toJSON(), listen[1]], function(tx, results) {
-				console.log('success:', results);
-			}, function(tx, error) {
-				console.log('error:', error);
-			});
-		});
+		executeSql(tx, 'SELECT * FROM songs', []);
+		executeSql(tx, 'SELECT * FROM listens', []);
 	});
+}
+
+function countListensByDate() {
+	db.transaction(function(tx) {
+		executeSql(tx, 'SELECT date, COUNT(id) FROM listens GROUP BY date', [], parseDates);
+	});
+}
+
+function parseDates(results) {
+	for (var i=0; i<results.rows.length; i++) {
+		var date = results.rows.item(i).date;
+		console.log('date', date, 'becomes', d3.isoParse(date));
+	}
+}
+
+function executeSql(tx, sql, vals, success_callback, failure_callback) {
+	tx.executeSql(sql, vals, success.bind(null, success_callback), failure.bind(null, failure_callback));
+}
+
+function success(callback, tx, results) {
+	if (print_results) {
+		console.log('success:');
+		printRows(results);
+  } else {
+		console.log('success:', results);
+  }
+	if (callback) callback(results);
+}
+
+function failure(callback, tx, error) {
+	console.log('error:', error);
+	if (callback) callback(error);
+}
+
+function printRows(results) {
+	for (var i=0; i<results.rows.length; i++) {
+		console.log(i, results.rows.item(i));
+	}
 }
